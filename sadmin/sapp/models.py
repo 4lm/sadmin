439ce1faa5dc9535877e7bb305073a9bee2e8016
Alexis Michaltsis <alexis@michaltsis.net>
2018-11-24 17:53:19 +0100

    Overwrite magic method __str__ in Student and Course model classes to return name string

--- sadmin/sapp/models.py
+++ sadmin/sapp/models.py
@@ -5,7 +5,13 @@ class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
 
 class Course(models.Model):
     name = models.CharField(max_length=60)
     students = models.ManyToManyField('Student')
+
+    def __str__(self):
+        return self.name
